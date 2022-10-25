1695cd4cdd5c050f8b6cab2dc0ece7ef5836fc25
Minoru Otani <otani@ccs.tsukuba.ac.jp>
2022-10-25 23:40:00 +0900

    fix install homebrew

--- install.sh
+++ install.sh
@@ -138,7 +138,8 @@ setup_homebrew() {
     if test ! "$(command -v brew)"; then
         info "Homebrew not installed. Installing."
         # Run as a login shell (non-interactive) so that the script doesn't pause for user input
-        curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash --login
+        #curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash --login
+        /bin/bash -c "$(https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     fi
 
     if [ "$(uname)" == "Linux" ]; then
